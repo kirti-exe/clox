a692d22bd7c11c2e37abd0a8104586fee1256c0d
kirti-exe <kirti.exe@gmail.com>
2026-09-08 17:01:08 +0530

    written code to scan token one at a time and also keep error in check

--- scanner.c
+++ scanner.c
@@ -17,3 +17,24 @@ void initScanner(const char* source){
     scanner.current = source;
     scanner.line = 1;
 }
+
+static bool isAtEnd(){
+    return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type){
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+    return token;
+}
+
+Token scanToken(){
+    scanner.start = scanner.current;
+
+    if(isAtEnd()) return makeToken(TOKEN_EOF);
+
+    return errorToken("Unexpected character.");
+}
